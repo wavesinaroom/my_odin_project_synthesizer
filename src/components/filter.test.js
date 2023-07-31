b82380a8f63e218933f458636c1b6c7450662618
wavesinaroom <jaureguij@javeriana.edu.co>
2023-07-30 20:35:53 -0500

    [Task] Test frequency slider -> values are right but can't pass test

--- src/components/filter.test.js
+++ src/components/filter.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom' 
-import {cleanup,render,screen} from "@testing-library/react";
+import {cleanup,fireEvent,render,screen} from "@testing-library/react";
 import Filter from "./filter";
 import Settings from "./settings.json"
 
@@ -12,4 +12,18 @@ it(`renders all input elements`,()=>{
 
   expect(screen.getAllByRole(`slider`).length).toBe(4);
   expect(screen.getAllByRole(`radio`).length).toBe(4);
+});
+
+it(`changes frequency slider value`, async()=>{
+  render(<Filter settings={Settings}/>);
+  const frequency = screen.getByRole(`slider`, {name:`frequency`});
+
+  expect(frequency.value).toBe(1000);
+  expect(Settings.filter.frequency).toBe(1000);
+
+  fireEvent.change(frequency, {target:{value:200}})
+
+  expect(frequency.value).toBe(200);
+  expect(Settings.filter.frequency).toBe(200);
+
 });
