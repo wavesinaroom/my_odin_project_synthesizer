02e8c56e3f952e1bea79fd159fb8a58ad7c07e21
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-02 17:09:13 -0500

    [Task] Sign up events

--- src/components/login.js
+++ src/components/login.js
@@ -5,6 +5,7 @@ import {useEffect} from 'react';
 const Login = () =>{
   const [user, setUser] = useState({email:'', password:''});
   const [exception, setException] = useState('');
+  const [signup, setSignup] = useState(false);
 
   function handleEmail(e){
     setUser({...user,email: e.target.value});
@@ -14,34 +15,74 @@ const Login = () =>{
     setUser({...user,password: e.target.value});
   }
 
-  async function handleSubmit(e){
+  function handleAccountExists(){
+    setSignup(!signup);
+  }
+
+  function handleNoAccount(){
+    setSignup(!signup);
+  }
+
+  async function handleLogin(e){
     e.preventDefault();
 
     const {data, error} = await supabase.auth.signInWithPassword(
       {email:user.email,
       password:user.password}
     )
-
-    if(error)
+    if(data)
+      console.log(`Logged in`);
+    else if(error)
       setException(error.message);
+  }
+
+  async function handleSignup(e){
+    e.preventDefault();
+
+    const [data, error] = await supabase.auth.signUp(
+      {email:user.email,
+        password:user.password}
+    );
+    if(data)
+      console.log(`Signed up`);
+    else if(error)
+      setException(error.message)
   }
 
   return(
     <>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Please enter your e-mail and password</legend> 
-          <label htmlFor='email'>E-mail:
-            <input value={user.email}  name='email' type='email' onChange={handleEmail} autoFocus required/>
-          </label>
-          <label>Password:
-            <input value={user.password} type='password' onChange={handlePassword} required/>
-          </label>
-          <p>{exception}</p>
-          <input value='Login' type='submit' id='submit'/>
-          <button>Signup</button>
-        </fieldset>
-      </form>
+      {signup?
+        <form onSubmit={handleSignup}>
+          <fieldset>
+            <legend>Please enter your e-mail and password to create your account</legend> 
+            <label htmlFor='email'>E-mail:
+              <input value={user.email}  name='email' type='email' onChange={handleEmail} autoFocus required/>
+            </label>
+            <label>Password:
+              <input value={user.password} type='password' onChange={handlePassword} required/>
+            </label>
+            <p>{exception}</p>
+            <input value='Sign up' type='submit' id='submit'/>
+            <button onClick={handleAccountExists}>Login</button>
+          </fieldset>
+        </form>
+        :
+        <form onSubmit={handleLogin}>
+          <fieldset>
+            <legend>Please enter your e-mail and password</legend> 
+            <label htmlFor='email'>E-mail:
+              <input value={user.email}  name='email' type='email' onChange={handleEmail} autoFocus required/>
+            </label>
+            <label>Password:
+              <input value={user.password} type='password' onChange={handlePassword} required/>
+            </label>
+            <p>{exception}</p>
+            <input value='Login' type='submit' id='submit'/>
+            <p>Don&apos;t have an account?</p>
+            <button onClick={handleNoAccount}>Signup</button>
+          </fieldset>
+        </form>
+      }
     </>
   )
 }
