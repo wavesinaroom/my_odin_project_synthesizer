54949d7d8cc8dc66b1cec8eebfb20d103c160201
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-02 15:41:06 -0500

    [Task] Login event

--- src/components/login.js
+++ src/components/login.js
@@ -3,44 +3,42 @@ import { useState } from 'react';
 import {useEffect} from 'react';
 
 const Login = () =>{
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [user, setUser] = useState({});
-
+  const [user, setUser] = useState({email:'', password:''});
 
   function handleEmail(e){
-    setEmail(e.target.value);
+    setUser({...user,email: e.target.value});
   }
 
   function handlePassword(e){
-    setPassword(e.target.value);
+    setUser({...user,password: e.target.value});
   }
 
-  function handleLogin(){
-    setUser({email:email, password:password});
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const {data, error} = await supabase.auth.signInWithPassword(
+      {email:user.email,
+      password:user.password}
+    )
+
+    if(error)
+      alert(error)
+
+    console.log(data);
   }
-
-  useEffect(()=>{
-    async function signInWithEmail(){
-      const {data,error} = await supabase.auth.signInWithPassword(user);
-    }
-
-    signInWithEmail();
-
-  },[user])
 
   return(
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>Please enter your e-mail and password</legend> 
           <label htmlFor='email'>E-mail:
-            <input value={email}  name='email' type='email' onChange={handleEmail} autoFocus required/>
+            <input value={user.email}  name='email' type='email' onChange={handleEmail} autoFocus required/>
           </label>
           <label>Password:
-            <input value={password} type='password' onChange={handlePassword} required/>
+            <input value={user.password} type='password' onChange={handlePassword} required/>
           </label>
-          <button onClick={handleLogin}>Login</button>
+          <input value='Login' type='submit' id='submit'/>
           <button>Signup</button>
         </fieldset>
       </form>
