83902a4566a6f60c678223799dca33d67f6b9b35
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-03 16:24:56 -0500

    [Task] Correct Sign up button mispell

--- src/components/login.test.js
+++ src/components/login.test.js
@@ -1,6 +1,8 @@
 import '@testing-library/jest-dom'
 import { cleanup, render, screen, fireEvent} from "@testing-library/react"; 
 import Login from "./login";
+import Settings from './settings.json';
+import Default from './default.json';
 
 beforeEach(()=>{
   cleanup();
@@ -12,14 +14,14 @@ it(`renders login form contents`,async()=>{
   expect(screen.getByRole(`group`, {name:`Please enter your e-mail and password`})).toBeInTheDocument();
   expect(screen.getByLabelText(`E-mail:`)).toBeInTheDocument();
   expect(screen.getByRole(`textbox`,{name:`E-mail:`})).toBeInTheDocument();
-  expect(screen.getByRole(`button`, {name:`Signup`})).toBeInTheDocument();
+  expect(screen.getByRole(`button`, {name:`Sign up`})).toBeInTheDocument();
   expect(screen.getByRole(`button`, {name:`Login`})).toBeInTheDocument();
 });
 
 it(`renders signup form contents on button click`, ()=>{
   render(<Login/>);
 
-  fireEvent.click(screen.getByRole(`button`, {name:`Signup`}));
+  fireEvent.click(screen.getByRole(`button`, {name:`Sign up`}));
 
   expect(screen.getByRole(`group`, {name:`Please enter e-mail and password to create an account`})).toBeInTheDocument();
   expect(screen.getByLabelText(`E-mail:`)).toBeInTheDocument();
@@ -33,10 +35,20 @@ it(`comes back to login from sign up form`,()=>{
 
   expect(screen.getByRole(`group`, {name:`Please enter your e-mail and password`})).toBeInTheDocument();
 
-    fireEvent.click(screen.getByRole(`button`, {name:`Signup`}));
+    fireEvent.click(screen.getByRole(`button`, {name:`Sign up`}));
     expect(screen.getByRole(`group`, {name:`Please enter e-mail and password to create an account`})).toBeInTheDocument();
 
   fireEvent.click(screen.getByRole(`button`, {name:`Login`}));
   
     expect(screen.getByRole(`group`, {name:`Please enter your e-mail and password`})).toBeInTheDocument();
 });
+
+it(`creates a new session for a new user`,()=>{
+  render(<Login/>);
+  
+  fireEvent.click(screen.getByRole(`button`, {name:`Sign up`}));
+
+  fireEvent.change(screen.getByRole(`textbox`, {name:`E-mail:`}), {target:{value:`jaureguij@javeriana.edu.co`}});
+  fireEvent.change(screen.getByLabelText(`Password:`), {target:{value:`Op2n1beethoven`}});
+  fireEvent.click(screen.getByRole(`button`), {name:`Sign up`});
+})
