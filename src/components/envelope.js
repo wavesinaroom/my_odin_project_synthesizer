952068e101f84ecf72db2986934f0580211c1770
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-09 11:32:50 -0500

    [Feature] Envelope rendering

---
+++ src/components/envelope.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const Envelope = ()=>{
+  const [attack, setAttack] = useState(0);
+  const [decay, setDecay] = useState(0);
+  const [sustain, setSustain] = useState(0);
+  const [release, setRelease] = useState(0);
+
+  return(
+    <>
+      <label>
+        <input value={attack} type='range' min='0' max='1' step='0.1'/>
+      </label>
+      <label>
+        <input value={decay} type='range' min='0' max='1' step='0.1'/>
+      </label>
+      <label>
+        <input value={sustain} type='range' min='0' max='1' step='0.1'/>
+      </label>
+      <label>
+        <input value={release} type='range' min='0' max='1' step='0.1'/>
+      </label>
+    </>
+  )
+}
