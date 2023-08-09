5f80d34049b988870aa8b0fd5e5764c6b03c5c40
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-09 12:10:27 -0500

    [Task] Remove import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Login from './components/login';
+import { useContext } from "react";
+import {profile} from './profile.js'
 
 function App() {
+  const userProfile = useContext(profile);
   return(
     <>
-      <Login/>
     </>
   )
 }
