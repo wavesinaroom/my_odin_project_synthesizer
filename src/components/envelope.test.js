37ba1953e75d24f879583f877c77266ee91b8abe
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-10 15:24:24 -0500

    [Feature] User interaction tests

--- src/components/envelope.test.js
+++ src/components/envelope.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { screen, render, cleanup} from "@testing-library/react";
+import { screen, render, cleanup, fireEvent} from "@testing-library/react";
 import Envelope from "./envelope";
 import Default from './default.json'
 
@@ -26,3 +26,35 @@ describe(`Rendering`, ()=>{
   })
 })
 
+describe(`Interaction`,()=>{
+  const profile = {info:Default};
+
+  it(`changes attack value in profile`, ()=>{
+    render(<Envelope settings={profile}/>);
+    const attack = screen.getByRole(`slider`,{name:`attack`});
+    
+    fireEvent.change(attack, {target:{value:0.5}});
+    expect(profile.info.envelope.a).toBe(`0.5`);
+  })
+  it(`changes decay value in profile`, ()=>{
+    render(<Envelope settings={profile}/>);
+    const decay = screen.getByRole(`slider`,{name:`decay`});
+    
+    fireEvent.change(decay, {target:{value:0.5}});
+    expect(profile.info.envelope.d).toBe(`0.5`);
+  })
+  it(`changes sustain value in profile`, ()=>{
+    render(<Envelope settings={profile}/>);
+    const sustain = screen.getByRole(`slider`,{name:`sustain`});
+    
+    fireEvent.change(sustain, {target:{value:0.5}});
+    expect(profile.info.envelope.s).toBe(`0.5`);
+  })
+  it(`changes release value in profile`, ()=>{
+    render(<Envelope settings={profile}/>);
+    const release = screen.getByRole(`slider`,{name:`release`});
+    
+    fireEvent.change(release, {target:{value:0.5}});
+    expect(profile.info.envelope.r).toBe(`0.5`);
+  })
+})
