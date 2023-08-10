53da0195fc30bfea246c1893cd31d0777634003c
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-10 17:46:11 -0500

    [Task] Rename settings.json into default.json

--- src/components/carrier.test.js
+++ src/components/carrier.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import {cleanup,render,screen, fireEvent} from '@testing-library/react';
 import Carrier from './carrier';
-import Settings from './settings.json'
+import Default from './default.json'
 
 beforeEach(()=>{
   cleanup();
@@ -10,13 +10,13 @@ beforeEach(()=>{
 describe(`Rendering`, ()=>{
 
   it(`renders waveform type radio buttons`,()=>{
-    render(<Carrier settings={Settings}/>);
+    render(<Carrier settings={Default}/>);
 
     expect(screen.getAllByRole(`radio`).length).toBe(4);
   });
 
   it(`checks radio buttons label match waveform types`,()=>{
-    render(<Carrier settings={Settings}/>);
+    render(<Carrier settings={Default}/>);
 
     expect(screen.getByRole(`radio`,{name:`sine`})).toBeInTheDocument(); 
     expect(screen.getByRole(`radio`,{name:`square`})).toBeInTheDocument(); 
@@ -25,30 +25,30 @@ describe(`Rendering`, ()=>{
   });
 
   it(`chooses a waveform and updates JSON file`,()=>{
-    render(<Carrier settings={Settings}/>);
+    render(<Carrier settings={Default}/>);
 
     fireEvent.click(screen.getByRole(`radio`,{name:`sine`}));
-    expect(Settings.carrier.type).toMatch(`sine`);
+    expect(Default.carrier.type).toMatch(`sine`);
   });
 
   it(`chooses one waveform only`,()=>{
-    render(<Carrier settings={Settings}/>);
+    render(<Carrier settings={Default}/>);
 
     fireEvent.click(screen.getByRole(`radio`,{name:`sine`}));
-    expect(Settings.carrier.type).toMatch(`sine`);
+    expect(Default.carrier.type).toMatch(`sine`);
 
     fireEvent.click(screen.getByRole(`radio`,{name:`square`}));
-    expect(Settings.carrier.type).toMatch(`square`);
-    expect(Settings.carrier.type).not.toMatch(`sine`);
+    expect(Default.carrier.type).toMatch(`square`);
+    expect(Default.carrier.type).not.toMatch(`sine`);
   })
 
   it(`changes volume level`,()=>{
-    render(<Carrier settings={Settings}/>);
+    render(<Carrier settings={Default}/>);
     const volume = screen.getByRole(`slider`,{name:`volume`});
-    expect(Settings.carrier.volume).toEqual(1);
+    expect(Default.carrier.volume).toEqual(1);
 
     fireEvent.change(volume, {target:{value:`0.32`}});
-    expect(Settings.carrier.volume).toEqual(-9.89700043360188);
+    expect(Default.carrier.volume).toEqual(-9.89700043360188);
     expect(volume.value).toBe(`0.32`);
   })
 });
