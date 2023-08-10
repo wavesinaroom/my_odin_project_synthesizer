53a6803f3eaf409104741165c960ef99dd4eb59b
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-10 14:49:04 -0500

    [Feature] Envelope component

---
+++ src/components/envelope.js
@@ -0,0 +1,48 @@
+import {useState} from "react";
+
+const Envelope = ({settings}) =>{
+  const [attack,setAttack] = useState(settings.info.envelope.a);
+  const [decay, setDecay] = useState(settings.info.envelope.d);
+  const [sustain, setSustain] = useState(settings.info.envelope.s); 
+  const [release, setRelease] = useState(settings.info.envelope.r);
+
+  function handleAttack(e){
+    setAttack(e.target.value);
+    settings.info.envelope.a = e.target.value;
+  }
+  function handleDecay(e){
+    setDecay(e.target.value);
+    settings.info.envelope.d = e.target.value;
+  }
+  function handleSustain(e){
+    setSustain(e.target.value);
+    settings.info.envelope.s = e.target.value;
+  }
+  function handleRelease(e){
+    setRelease(e.target.value);
+    settings.info.envelope.r = e.target.value;
+  }
+
+  return(
+    <>
+      <label>
+        <input value={attack} type='range' min='0' max='1' step='0.1' onChange={handleAttack}/>      
+        attack
+      </label>
+      <label>
+        <input value={decay} type='range' min='0' max='1' step='0.1' onChange={handleDecay}/>      
+        attack
+      </label>
+      <label>
+        <input value={sustain} type='range' min='0' max='1' step='0.1' onChange={handleSustain}/>      
+        attack
+      </label>
+      <label>
+        <input value={release} type='range' min='0' max='1' step='0.1' onChange={handleRelease}/>      
+        attack
+      </label>
+    </>
+  );
+}
+
+export default Envelope;
