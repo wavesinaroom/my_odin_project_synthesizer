ce4dc3a9f5e06368d9856653c054f5ccc8c8f275
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-10 17:25:09 -0500

    [Fix] Label inputs

--- src/components/lfo.js
+++ src/components/lfo.js
@@ -20,20 +20,45 @@ const LFO = ({settings}) =>{
     <>
       <fieldset>
         <legend>Type</legend>
-          <input value='sine' type='radio' onChange={handleType} checked={true}/> 
-          <input value='square' type='radio' onChange={handleType}/> 
-          <input value='sawtooth' type='radio' onChange={handleType}/> 
-          <input value='triangle' type='radio' onChange={handleType}/> 
+          <label>
+            <input value='sine' type='radio' onChange={handleType} checked={true}/> 
+            sine
+          </label>
+          <label>
+            <input value='square' type='radio' onChange={handleType}/> 
+            square
+          </label>
+          <label>
+            <input value='sawtooth' type='radio' onChange={handleType}/> 
+            sawtooth
+          </label>
+          <label>
+            <input value='triangle' type='radio' onChange={handleType}/> 
+            triangle
+          </label>
       </fieldset>
       <label>
         <input value={frequency} type='range' onChange={handleFrequency} min='0.0001' max='20' step='1'/>
+        frequency
       </label>
       <fieldset>
         <legend>Target</legend>
-          <input value='carrier' type='radio' onChange={handleTarget}/>
-          <input value='modulator' type='radio' onChange={handleTarget}/>
-          <input value='envelope' type='radio' onChange={handleTarget}/>
-          <input value='none' type='radio' onChange={handleTarget}/>
+          <label>
+            <input value='carrier' type='radio' onChange={handleTarget}/>
+            carrier
+          </label>
+          <label>
+            <input value='modulator' type='radio' onChange={handleTarget}/>
+            modulator
+          </label>
+          <label>
+            <input value='envelope' type='radio' onChange={handleTarget}/>
+            envelope
+          </label>
+          <label>
+            <input value='none' type='radio' onChange={handleTarget}/>
+            none
+          </label>
       </fieldset>
     </>
   )
--- src/components/lfo.test.js
+++ src/components/lfo.test.js
@@ -9,10 +9,27 @@ beforeEach(()=>{
 
 describe(`Rendering`,()=>{
   const profile = {info:Default};
+
   it(`renders the right amount of elements`,()=>{
     render(<LFO settings={profile}/>);
 
     expect(screen.getAllByRole(`radio`).length).toBe(8);
     expect(screen.getAllByRole(`slider`).length).toBe(1);
-  })
-})
+  });
+
+  it(`renders the right input values`,()=>{
+    render(<LFO settings={profile}/>);
+
+    expect(screen.getByRole(`radio`,{name:`sine`})).toBeInTheDocument();
+    expect(screen.getByRole(`radio`,{name:`square`})).toBeInTheDocument();
+    expect(screen.getByRole(`radio`,{name:`sawtooth`})).toBeInTheDocument();
+    expect(screen.getByRole(`radio`,{name:`triangle`})).toBeInTheDocument();
+
+    expect(screen.getByRole(`slider`,{name:`frequency`})).toBeInTheDocument();
+
+    expect(screen.getByRole(`radio`,{name:`carrier`})).toBeInTheDocument();
+    expect(screen.getByRole(`radio`,{name:`modulator`})).toBeInTheDocument();
+    expect(screen.getByRole(`radio`,{name:`envelope`})).toBeInTheDocument();
+    expect(screen.getByRole(`radio`,{name:`none`})).toBeInTheDocument();
+  });
+});
