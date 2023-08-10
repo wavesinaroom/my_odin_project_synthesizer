a25209debdd265725a8000f7193e44fff5f3c064
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-10 15:16:35 -0500

    [Task] Check rendered labels

--- src/components/envelope.test.js
+++ src/components/envelope.test.js
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom'
 import { screen, render, cleanup} from "@testing-library/react";
 import Envelope from "./envelope";
 import Default from './default.json'
@@ -13,6 +14,15 @@ describe(`Rendering`, ()=>{
     render(<Envelope settings={profile}/>);
 
     expect(screen.getAllByRole(`slider`).length).toBe(4);
+  });
+
+  it(`renders the right labels`,()=>{
+    render(<Envelope settings={profile}/>);
+
+    expect(screen.getByLabelText(`attack`)).toBeInTheDocument();
+    expect(screen.getByLabelText(`decay`)).toBeInTheDocument();
+    expect(screen.getByLabelText(`sustain`)).toBeInTheDocument();
+    expect(screen.getByLabelText(`release`)).toBeInTheDocument();
   })
 })
 
