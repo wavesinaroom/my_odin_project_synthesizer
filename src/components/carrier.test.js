caffc8e3244a760901fc7b14a1760898a94dc7a0
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-11 16:55:33 -0500

    [Taks] Render component inside a context, update json file name

--- src/components/carrier.test.js
+++ src/components/carrier.test.js
@@ -2,22 +2,30 @@ import '@testing-library/jest-dom';
 import {cleanup,render,screen, fireEvent} from '@testing-library/react';
 import Carrier from './carrier';
 import Default from './default.json'
+import { Profile } from './profile';
 
 beforeEach(()=>{
   cleanup();
-})
+});
 
 describe(`Rendering`, ()=>{
   const profile = {info:Default};
-
   it(`renders waveform type radio buttons`,()=>{
-    render(<Carrier settings={profile}/>);
+    render(
+      <Profile.Provider value={profile}>
+        <Carrier/>
+      </Profile.Provider>
+    )
 
     expect(screen.getAllByRole(`radio`).length).toBe(4);
   });
 
   it(`checks radio buttons label match waveform types`,()=>{
-    render(<Carrier settings={profile}/>);
+    render(
+      <Profile.Provider value={profile}>
+        <Carrier/>
+      </Profile.Provider>
+    )
 
     expect(screen.getByRole(`radio`,{name:`sine`})).toBeInTheDocument(); 
     expect(screen.getByRole(`radio`,{name:`square`})).toBeInTheDocument(); 
@@ -27,21 +35,29 @@ describe(`Rendering`, ()=>{
 });
 
 describe(`Interaction`,()=>{
-  const profile = {info:Default};
+  const profile = {settings:Default};
   it(`chooses a waveform and updates JSON file`,()=>{
-    render(<Carrier settings={profile}/>);
+    render(
+      <Profile.Provider value={profile}>
+        <Carrier/>
+      </Profile.Provider>
+    )
 
-    expect(profile.info.carrier.type).toMatch(`sine`);  
+    expect(profile.settings.carrier.type).toMatch(`sine`);  
     fireEvent.click(screen.getByRole(`radio`,{name:`square`}));
-    expect(profile.info.carrier.type).toMatch(`square`);
+    expect(profile.settings.carrier.type).toMatch(`square`);
   });
 
   it(`changes volume level`,()=>{
-    render(<Carrier settings={profile}/>);
+    render(
+      <Profile.Provider value={profile}>
+        <Carrier/>
+      </Profile.Provider>
+    )
     const volume = screen.getByRole(`slider`,{name:`volume`});
-    expect(profile.info.carrier.volume).toEqual(1);
+    expect(profile.settings.carrier.volume).toEqual(1);
 
     fireEvent.change(volume, {target:{value:`0.32`}});
-    expect(profile.info.carrier.volume).toEqual(`-9.90`);
-  })
+    expect(profile.settings.carrier.volume).toEqual(`-9.90`);
+  });
 });
