12a04dca3bfd667197b71619dfbcdbb6aa2d0554
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-12 19:35:19 -0500

    [Task] Remove states

--- src/components/envelope.js
+++ src/components/envelope.js
@@ -1,44 +1,37 @@
-import {useState} from "react";
+import {useContext} from "react";
+import {Profile} from './profile';
 
-const Envelope = ({settings}) =>{
-  const [attack,setAttack] = useState(settings.info.envelope.a);
-  const [decay, setDecay] = useState(settings.info.envelope.d);
-  const [sustain, setSustain] = useState(settings.info.envelope.s); 
-  const [release, setRelease] = useState(settings.info.envelope.r);
-
+const Envelope = () =>{
+  const profile = useContext(Profile)
   function handleAttack(e){
-    setAttack(e.target.value);
-    settings.info.envelope.a = e.target.value;
+    profile.settings.envelope.a = e.target.value;
   }
   function handleDecay(e){
-    setDecay(e.target.value);
-    settings.info.envelope.d = e.target.value;
+    profile.settings.envelope.d = e.target.value;
   }
   function handleSustain(e){
-    setSustain(e.target.value);
-    settings.info.envelope.s = e.target.value;
+    profile.settings.envelope.s = e.target.value;
   }
   function handleRelease(e){
-    setRelease(e.target.value);
-    settings.info.envelope.r = e.target.value;
+    profile.settings.envelope.r = e.target.value;
   }
 
   return(
     <>
       <label>
-        <input value={attack} type='range' min='0' max='1' step='0.1' onChange={handleAttack}/>      
+        <input value={profile.settings.envelope.a} type='range' min='0' max='1' step='0.1' onChange={handleAttack}/>      
         attack
       </label>
       <label>
-        <input value={decay} type='range' min='0' max='1' step='0.1' onChange={handleDecay}/>      
+        <input value={profile.settings.envelope.d} type='range' min='0' max='1' step='0.1' onChange={handleDecay}/>      
         decay
       </label>
       <label>
-        <input value={sustain} type='range' min='0' max='1' step='0.1' onChange={handleSustain}/>      
+        <input value={profile.settings.envelope.s} type='range' min='0' max='1' step='0.1' onChange={handleSustain}/>      
         sustain
       </label>
       <label>
-        <input value={release} type='range' min='0' max='1' step='0.1' onChange={handleRelease}/>      
+        <input value={profile.settings.envelope.r} type='range' min='0' max='1' step='0.1' onChange={handleRelease}/>      
         release
       </label>
     </>
