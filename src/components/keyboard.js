c31e8495ff612a93a06a69895decad49ac53c236
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-15 10:40:07 -0500

    [Task] Draft keys svg circles

---
+++ src/components/keyboard.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Keyboard = () =>{
+  return(
+    <>
+      <svg>
+        <circle id="c" cx="0" cy="0" r="100"/>
+        <circle id="cs" cx="0" cy="0" r="100"/>
+        <circle id="d" cx="0" cy="0" r="100"/>
+        <circle id="ds" cx="0" cy="0" r="100"/>
+        <circle id="e" cx="0" cy="0" r="100"/>
+        <circle id="f" cx="0" cy="0" r="100"/>
+        <circle id="fs" cx="0" cy="0" r="100"/>
+        <circle id="g" cx="0" cy="0" r="100"/>
+        <circle id="gs" cx="0" cy="0" r="100"/>
+        <circle id="a" cx="0" cy="0" r="100"/>
+        <circle id="as" cx="0" cy="0" r="100"/>
+        <circle id="b" cx="0" cy="0" r="100"/>
+        <circle id="c" cx="0" cy="0" r="100"/>
+      </svg>
+    </>
+  )
+}
