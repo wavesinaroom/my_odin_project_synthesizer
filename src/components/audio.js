85da648ea7d785d8bc151befbe5b24ce27653fd2
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-15 17:41:28 -0500

    [Task] Refactor useEffect code to a new script

---
+++ src/components/audio.js
@@ -0,0 +1,73 @@
+ const Audio = ()=>{
+  const audioCtx = window.AudioContext;
+  const carrier = audioCtx.createOscillator();
+  const modulator = audioCtx.createOscillator();
+  const lfo = audioCtx.createOscillator();
+  const envelope = new GainNode(audioCtx);
+  const filter = new BiquadFilterNode(audioCtx);
+
+  modulator.connect(carrier).connect(envelope).filter.connect(audioCtx.destination);
+
+  function setCarrier(settings){
+    carrier.type = settings.type;
+    carrier.detune = settings.detune;
+  }
+
+  function setModulator(settings){
+    modulator.type =  settings.type;
+  }
+
+  function setLFO(settings){
+    lfo.type = settings.type;
+    lfo.frequency = settings.frequency;
+  }
+
+  function setEnvelope(settings){
+    envelope.gain = settings.volume;
+  }
+
+  function setFilter(settings){
+    filter.frequency = settings.frequency;
+    filter.detune = settings.detune;
+    filter.q = settings.q;
+    filter.volume = settings.volume;
+    filter.type = settings.type;
+  }
+
+  function envelopeOn(settings){
+    let now = audioCtx.currentTime;
+
+    envelope.cancelScheduledValues(0);
+    envelope.setValueAtTime(0,now);
+    envelope.linearRampToValueAtTime(1, now + settings.a);
+    envelope.linearRampToValueAtTime(settings.s, now + settings.a + settings.d);
+  }
+
+  function envelopeOff(settings){
+    let now = audioCtx.currentTime;
+
+    envelope.cancelScheduledValues(0);
+    envelope.setValueAtTime(envelope.value,now);
+    envelope.linearRampToValueAtTime(0, now + settings.r);
+  }
+
+   function plugLFO(settings){
+     lfo.disconnect();
+
+    switch(settings.lfo.target){
+      case "carrier":
+        lfo.connect(carrier);
+        break;
+      case "modulator":
+        lfo.connect(modulator)
+        break;
+      case "envelope":
+        lfo.connect(envelope);
+        break;
+      default:
+        break;
+      }
+   }
+}
+
+export default Audio;
--- src/components/body.js
+++ src/components/body.js
@@ -5,41 +5,26 @@ import Modulator from './modulator'
 import Filter from './filter'
 import LFO from './lfo'
 import Envelope from './envelope'
+import Audio from './audio'
 
 const Body = ()=>{
   const profile = useContext(Profile)
 
   useEffect(()=>{
-    const audioCtx = window.AudioContext;
-    const carrier = audioCtx.createOscillator();
-    const modulator = audioCtx.createOscillator();
-    const lfo = audioCtx.createOscillator();
-    const envelope = new GainNode(audioCtx);
-    const filter = new BiquadFilterNode(audioCtx);
     
-    setCarrier(profile.settings.carrier, carrier);
-    setModulator(profile.settings.modulator, modulator);
-    setLFO(profile.settings.lfo, lfo);
-    setEnvelope(profile.settings.envelope, envelope);
-    setFilter(profile.settings.filter, filter);
-
-    modulator.connect(carrier).connect(envelope).filter.connect(audioCtx.destination);
-
-    switch(profile.settings.lfo.target){
-      case "carrier":
-        lfo.connect(carrier);
-        break;
-      case "modulator":
-        lfo.connect(modulator)
-        break;
-      case "envelope":
-        lfo.connect(envelope);
-        break;
-      default:
-        break;
-    }
+    Audio.setCarrier(profile.settings.carrier);
+    Audio.setModulator(profile.settings.modulator);
+    Audio.setLFO(profile.settings.lfo);
+    Audio.setEnvelope(profile.settings.envelope);
+    Audio.setFilter(profile.settings.filter);
 
   },[profile])
+
+  useEffect(()=>{
+
+    Audio.plugLFO(profile.settings.lfo.target);
+
+  },[profile.settings.lfo.target])
 
   return(
     <>
@@ -56,45 +41,3 @@ const Body = ()=>{
 
 export default Body;
 
-function setCarrier(settings, node){
-  node.type = settings.type;
-  node.detune = settings.detune;
-}
-
-function setModulator(settings, node){
-  node.type =  settings.type;
-}
-
-function setLFO(settings, node){
-  node.type = settings.type;
-  node.frequency = settings.frequency;
-}
-
-function setEnvelope(settings, node){
-  node.gain = settings.volume;
-}
-
-function setFilter(settings, node){
-  node.frequency = settings.frequency;
-  node.detune = settings.detune;
-  node.q = settings.q;
-  node.volume = settings.volume;
-  node.type = settings.type;
-}
-
-function envelopeOn(settings, node, context){
-  let now = context.currentTime;
-
-  node.cancelScheduledValues(0);
-  node.setValueAtTime(0,now);
-  node.linearRampToValueAtTime(1, now + settings.a);
-  node.linearRampToValueAtTime(settings.s, now + settings.a + settings.d);
-}
-
-function envelopeOff(settings, node, context){
-  let now = context.currentTime;
-
-  node.cancelScheduledValues(0);
-  node.setValueAtTime(node.value,now);
-  node.linearRampToValueAtTime(0, now + settings.r);
-}
