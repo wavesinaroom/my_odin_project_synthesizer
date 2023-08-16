53fa38e0259d22296d71332a69c8eaca5a585309
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-16 12:35:15 -0500

    [Task] Update radio buttons according to profile settings on component first render

--- src/components/carrier.js
+++ src/components/carrier.js
@@ -9,6 +9,10 @@ const Carrier = () =>{
     profile.settings.carrier.type = e.target.value;
   }
 
+  function handleDetune(e){
+    profile.settings.carrier.detune = e.target.value;
+  }
+
   useEffect(()=>{
     Audio.setCarrier(profile.settings.carrier);
   }, [profile.settings.carrier])
@@ -18,24 +22,24 @@ const Carrier = () =>{
       <fieldset>
         <legend>Waveform</legend>
           <label>
-           <input value='sine' name='sine' type='radio' onChange={handleWaveformChoice} checked={true}/>
+           <input value='sine' name='sine' type='radio' onChange={handleWaveformChoice} checked={profile.settings.carrier.type === 'sine'}/>
             sine
           </label>
           <label>
-           <input value='square' name='square' type='radio' onChange={handleWaveformChoice}/>
+           <input value='square' name='square' type='radio' onChange={handleWaveformChoice} checked={profile.settings.carrier.type === 'square'}/>
             square
           </label>
           <label>
-           <input value='triangle' name='triangle' type='radio' onChange={handleWaveformChoice}/>
+           <input value='triangle' name='triangle' type='radio' onChange={handleWaveformChoice} checked={ profile.settings.carrier.type === 'triangle' }/>
             triangle
           </label>
           <label>
-           <input value='sawtooth' name='sawtooth' type='radio' onChange={handleWaveformChoice}/>
+           <input value='sawtooth' name='sawtooth' type='radio' onChange={handleWaveformChoice} checked={profile.settings.carrier.type === 'sawtooth'}/>
             sawtooth
           </label>
       </fieldset>
       <label>
-        <input value={profile.settings.carrier.volume} name='volume' type='range' min="0.0001" step="1" max="1" onChange={handleVolumeChange}/>
+        <input value={profile.settings.carrier.detune} name='detune' type='range' min="0.0001" step="1" max="100" onChange={handleDetune}/>
         volume
       </label>
     </>
