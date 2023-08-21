ac534ecf42f6117da01eb66956106eca5a1037a3
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-21 17:38:08 -0500

    [Task] Make sure code compiles correctly without a test suite

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,10 +1,10 @@
  const Audio = (()=>{
-  const audioCtx = window.AudioContext;
-  const carrier = audioCtx.createOscillator();
-  const modulator = audioCtx.createOscillator();
-  const lfo = audioCtx.createOscillator();
-  const envelope = audioCtx.createGain();
-  const filter = audioCtx.createBiqueadFilter();
+  const audioCtx = new AudioContext();
+  const carrier = new OscillatorNode(audioCtx);
+  const modulator = new OscillatorNode(audioCtx);
+  const lfo = new OscillatorNode(audioCtx);
+  const envelope = new GainNode(audioCtx);
+  const filter = new BiquadFilterNode(audioCtx);
 
   modulator.connect(carrier).connect(envelope).filter.connect(audioCtx.destination);
 
