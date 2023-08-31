030aa59da09857478613675b25c600c2b8e5378a
wavesinaroom <jaureguij@javeriana.edu.co>
2023-08-31 10:15:21 -0500

    [Task] Solve conflicting merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { useContext } from "react";
-import {profile} from './profile.js'
+import Carrier from "./components/carrier.js";
+import Modulator from "./components/modulator";
+import LFO from "./components/lfo";
+import Filter from "./components/filter";
+import Envelope from "./components/envelope";
+import Keyboard from "./components/keyboard";
+import Login from "./components/login";
+import Logout from "./components/logout";
+import {Profile} from './components/profile.js'
+import Default from "./components/default.json"
+import {useState} from "react";
 
 function App() {
-  const userProfile = useContext(profile);
+  const [signed,setSigned] = useState(false);
+
   return(
     <>
+      <Profile.Provider value={{settings:Default}}>
+    
+      </Profile.Provider>
     </>
-  )
+  );
 }
 
 export default App;
