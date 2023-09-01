34fe1a3c016eca97f224df8962fe6ec52a0f4d64
wavesinaroom <jaureguij@javeriana.edu.co>
2023-09-01 10:07:09 -0500

    [Fix] Make sure carrier outputs sound with the most basic nodes connection possible

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,82 +1,86 @@
- const Audio = (()=>{
-   const audioCtx = new AudioContext();
-   const carrier = new OscillatorNode(audioCtx);
-   const modulator = new OscillatorNode(audioCtx);
-   const lfo = new OscillatorNode(audioCtx);
-   const envelope = new GainNode(audioCtx);
-   const filter = new BiquadFilterNode(audioCtx);
- 
-   modulator.connect(carrier)
-   carrier.connect(envelope)
-   filter.connect(audioCtx.destination);
+const Audio = (()=>{
+  const audioCtx = new AudioContext();
+  const carrier = new OscillatorNode(audioCtx);
+  const modulator = new OscillatorNode(audioCtx);
+  const lfo = new OscillatorNode(audioCtx);
+  const envelope = new GainNode(audioCtx);
+  const filter = new BiquadFilterNode(audioCtx);
 
-   function setCarrier(settings){
-     carrier.type = settings.type;
-     carrier.detune.setValueAtTime(settings.detune, 0);
+  carrier.connect(envelope).connect(audioCtx.destination);
+  carrier.start();
+
+  function setCarrier(settings){
+    carrier.type = settings.type;
+    carrier.detune.setValueAtTime(settings.detune, 0);
+  }
+
+  function setCarrierFrequency(frequency){
+    carrier.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+  }
+
+  function setModulator(settings){
+    modulator.type =  settings.type;
+  }
+
+  function setLFO(settings){
+    lfo.type = settings.type;
+    lfo.frequency = settings.frequency;
+  }
+
+  function setEnvelope(settings){
+    envelope.gain = settings.volume;
+  }
+
+  function setFilter(settings){
+    filter.frequency = settings.frequency;
+    filter.detune = settings.detune;
+    filter.q = settings.q;
+    filter.volume = settings.volume;
+    filter.type = settings.type;
+  }
+
+  function envelopeOn(settings){
+    envelope.gain.cancelScheduledValues(0);
+    envelope.gain.setValueAtTime(0,audioCtx.currentTime);
+    envelope.gain.linearRampToValueAtTime(3, audioCtx.currentTime + settings.a);
+    envelope.gain.linearRampToValueAtTime(settings.s, audioCtx.currentTime + settings.a + settings.d);
+  }
+
+  function envelopeOff(settings){
+    envelope.gain.cancelScheduledValues(0);
+    envelope.gain.setValueAtTime(envelope.gain.value,audioCtx.currentTime);
+    envelope.gain.linearRampToValueAtTime(0, audioCtx.currentTime + settings.r);
+  }
+
+  function plugLFO(settings){
+    lfo.disconnect();
+
+    switch(settings.lfo.target){
+      case "carrier":
+        lfo.connect(carrier);
+        break;
+      case "modulator":
+        lfo.connect(modulator)
+        break;
+      case "envelope":
+        lfo.connect(envelope);
+        break;
+      default:
+        break;
+     }
    }
- 
-   function setModulator(settings){
-     modulator.type =  settings.type;
+
+   return {
+     setCarrier,
+     setCarrierFrequency,
+     setModulator,
+     setFilter,
+     setEnvelope,
+     setLFO,
+     envelopeOn,
+     envelopeOff,
+     plugLFO
    }
- 
-   function setLFO(settings){
-     lfo.type = settings.type;
-     lfo.frequency = settings.frequency;
-   }
- 
-   function setEnvelope(settings){
-     envelope.gain = settings.volume;
-   }
- 
-   function setFilter(settings){
-     filter.frequency = settings.frequency;
-     filter.detune = settings.detune;
-     filter.q = settings.q;
-     filter.volume = settings.volume;
-     filter.type = settings.type;
-   }
- 
-   function envelopeOn(settings){
-     envelope.cancelScheduledValues(0);
-     envelope.setValueAtTime(0,audioCtx.currentTime);
-     envelope.linearRampToValueAtTime(1, audioCtx.currentTime + settings.a);
-     envelope.linearRampToValueAtTime(settings.s, audioCtx.currentTime + settings.a + settings.d);
-   }
- 
-   function envelopeOff(settings){
-     envelope.cancelScheduledValues(0);
-     envelope.setValueAtTime(envelope.value,audioCtx.currentTime);
-     envelope.linearRampToValueAtTime(0, audioCtx.currentTime + settings.r);
-   }
- 
-    function plugLFO(settings){
-      lfo.disconnect();
- 
-     switch(settings.lfo.target){
-       case "carrier":
-         lfo.connect(carrier);
-         break;
-       case "modulator":
-         lfo.connect(modulator)
-         break;
-       case "envelope":
-         lfo.connect(envelope);
-         break;
-       default:
-         break;
-       }
-    }
- 
-    return {
-      setCarrier,
-      setModulator,
-      setFilter,
-      setEnvelope,
-      setLFO,
-      envelopeOn,
-      envelopeOff,
-      plugLFO
-    }
  })();
 
 export default Audio;
