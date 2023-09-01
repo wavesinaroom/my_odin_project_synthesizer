05a6f892b7ed5ce622a9c471fa6f0cbcd6c29b1d
wavesinaroom <jaureguij@javeriana.edu.co>
2023-09-01 16:00:26 -0500

    [Task] Bring changes from envelope branch manually

--- src/components/__tests__/envelope.test.js
+++ src/components/__tests__/envelope.test.js
@@ -27,6 +27,7 @@ describe(`Rendering`, ()=>{
     expect(screen.getByLabelText(`decay`)).toBeInTheDocument();
     expect(screen.getByLabelText(`sustain`)).toBeInTheDocument();
     expect(screen.getByLabelText(`release`)).toBeInTheDocument();
+    expect(screen.getByLabelText(`volume`)).toBeInTheDocument();
   });
 
   it(`renders JSON profile values`,()=>{
@@ -40,6 +41,7 @@ describe(`Rendering`, ()=>{
     expect(screen.getByLabelText(`decay`).value).toBe(`${profile.settings.envelope.d}`);
     expect(screen.getByLabelText(`sustain`).value).toBe(`${profile.settings.envelope.s}`);
     expect(screen.getByLabelText(`release`).value).toBe(`${profile.settings.envelope.r}`);
+    expect(screen.getByLabelText(`volume`).value).toBe(`${profile.settings.envelope.gain}`);
   })
 })
 
@@ -55,8 +57,8 @@ describe(`Interaction`,()=>{
 
     const slider = screen.getByRole(`slider`,{name:`attack`});
     
-    expect(profile.settings.envelope.a).toBe(0);
-    expect(slider.value).toBe(`0`);
+    expect(profile.settings.envelope.a).toBe(0.0001);
+    expect(slider.value).toBe(`0.0001`);
 
     fireEvent.change(slider, {target:{value:0.5}});
 
@@ -74,8 +76,8 @@ describe(`Interaction`,()=>{
 
     const slider = screen.getByRole(`slider`,{name:`decay`});
     
-    expect(profile.settings.envelope.d).toBe(0);
-    expect(slider.value).toBe(`0`);
+    expect(profile.settings.envelope.d).toBe(0.0001);
+    expect(slider.value).toBe(`0.0001`);
 
     fireEvent.change(slider, {target:{value:0.5}});
 
@@ -93,8 +95,8 @@ describe(`Interaction`,()=>{
 
     const slider = screen.getByRole(`slider`,{name:`sustain`});
 
-    expect(profile.settings.envelope.s).toBe(0);
-    expect(slider.value).toBe(`0`);
+    expect(profile.settings.envelope.s).toBe(0.0001);
+    expect(slider.value).toBe(`0.0001`);
 
     fireEvent.change(slider, {target:{value:0.5}});
 
@@ -113,12 +115,31 @@ describe(`Interaction`,()=>{
 
     const slider = screen.getByRole(`slider`,{name:`release`});
 
-    expect(profile.settings.envelope.r).toBe(0);
-    expect(slider.value).toBe(`0`);
+    expect(profile.settings.envelope.r).toBe(0.0001);
+    expect(slider.value).toBe(`0.0001`);
 
     fireEvent.change(slider, {target:{value:0.5}});
 
     expect(profile.settings.envelope.r).toBe(`0.5`);
+    expect(slider.value).toBe(`0.5`)
+    expect(Audio.setEnvelope).toBeCalled();
+  })
+
+  it(`changes gain value in profile`, ()=>{
+    render(
+      <Profile.Provider value={profile}>
+        <Envelope/>
+      </Profile.Provider>
+    )
+
+    const slider = screen.getByRole(`slider`,{name:`volume`});
+
+    expect(profile.settings.envelope.gain).toBe(1);
+    expect(slider.value).toBe(`1`);
+
+    fireEvent.change(slider, {target:{value:0.5}});
+
+    expect(profile.settings.envelope.gain).toBe(`0.5`);
     expect(slider.value).toBe(`0.5`)
     expect(Audio.setEnvelope).toBeCalled();
   })
