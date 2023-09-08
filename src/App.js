0ccae633a232509cef9acc4f7a79d8ec284ec0ad
wavesinaroom <jaureguij@javeriana.edu.co>
2023-09-08 10:12:33 -0500

    [Task] Render circles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import Keyboard from './components/keyboard'
+import { Profile } from './components/profile';
+import Default from './components/default.json'
 
 function App() {
-  return;
+  const profile = {settings: Default}
+  return(
+    <>
+      <Profile.Provider value={profile}>
+        <Keyboard/>
+      </Profile.Provider>
+    </>
+  )
 }
 
 export default App;
