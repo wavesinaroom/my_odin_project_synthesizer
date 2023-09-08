7dc28f54a312c3012364786e4692c1ed4ddc4f6f
wavesinaroom <jaureguij@javeriana.edu.co>
2023-09-08 11:00:44 -0500

    [Task] Fit circles into a rombus

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -21,19 +21,19 @@ const Keyboard = () =>{
         <p>Hover over the circles to play some sound</p>
       </details>
       <svg viewBox="0 0 1500 1500">
-        <circle id="261.63" cx="100" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="277.10" cx="200" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="293.66" cx="300" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="311.13" cx="400" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="329.63" cx="500" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="349.32" cx="600" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="369.99" cx="700" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="392" cx="800" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="415.3" cx="900" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="440" cx="1000" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="466.16" cx="1100" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="493.88" cx="1200" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
-          <circle id="522.6" cx="1300" cy="750" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="261.63" cx="100" cy="220" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="277.10" cx="200" cy="157.5" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="293.66" cx="300" cy="95" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="311.13" cx="400" cy="30" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="329.63" cx="500" cy="95" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="349.32" cx="600" cy="157.5" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="369.99" cx="700" cy="220" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="392" cx="600" cy="282.5" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="415.3" cx="500" cy="345" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="440" cx="400" cy="407.5" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="466.16" cx="200" cy="282.5" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="493.88" cx="300" cy="345" r="30" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
+          <circle id="522.6" cx="100" cy="282.5" r="20" onMouseEnter={handleMouseEnter} onMouseOut={handleMouseOut}/>
       </svg>
     </>
   )
